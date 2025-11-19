b9955a6a9b5f54af4055761681f8a7bb33255b66
Kvaaa22 <kvaaaa5a@gmail.com>
2025-11-19 11:01:14 +0700

    11 Завершение работы функции MainScreen и исправление ошибки в файле Product.kt

--- app/src/main/java/com/example/roomdemo/Product.kt
+++ app/src/main/java/com/example/roomdemo/Product.kt
@@ -4,19 +4,17 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+@Entity(tableName = "products")
 class Product {
-    @Entity(tableName = "products")
-    class Product {
         @PrimaryKey(autoGenerate = true)
         @ColumnInfo(name = "productId")
         var id: Int = 0
         @ColumnInfo(name = "productName")
         var productName: String = ""
         var quantity: Int = 0
-        constructor()
+
         constructor(productname: String, quantity: Int) {
             this.productName = productname
             this.quantity = quantity
         }
-    }
 }
