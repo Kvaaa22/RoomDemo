c6215854c01214431bcf15fc3fe4271d258fca85
Kvaaa22 <kvaaaa5a@gmail.com>
2025-11-19 11:51:51 +0700

    12 чуть поправила версии ksp и конструктов в product.kt

--- app/src/main/java/com/example/roomdemo/Product.kt
+++ app/src/main/java/com/example/roomdemo/Product.kt
@@ -12,7 +12,7 @@ class Product {
         @ColumnInfo(name = "productName")
         var productName: String = ""
         var quantity: Int = 0
-
+        constructor()
         constructor(productname: String, quantity: Int) {
             this.productName = productname
             this.quantity = quantity
